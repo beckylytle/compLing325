49149f96e04f6c006f9ce4bdc2fbc9029f1e4c0d
Becky Lynn Lytle <rlytle@haverford.edu>
2017-03-19 15:09:24 -0400

    finished lab5

--- lab5_lytle.py
+++ lab5_lytle.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import nltk
 from nltk.corpus import treebank
 from nltk.tree import Tree
@@ -37,23 +38,51 @@ in the Tree and recreate it with the component Adjective and Noun in the opposit
 
 def S_Transfer(tree,v1,v2):
     #i'm assuming this is a binary tree and idk if that is right
-    if tree[0].label() == v1 and tree[1].label() == v2:
+    #update: it is not a binary tree so u need to fix this lol
+    for i in range(0,len(tree)):
+        #check i & i+1
+        if i!=len(tree)-1 and tree[i].label() in v1 and tree[i+1].label() in v2:
         #switch!
-        left = tree[0]
-        right = tree[1]
-        tree[0] = right
-        tree[1] = left
-    if len(tree[0])>1:
-        tree[0] = S_Transfer(tree[0],v1,v2)
-        #recursive call
-    if len(tree[1])>1:
-        tree[1] = S_Transfer(tree[1],v1,v2)
+            left = tree[i]
+            right = tree[i+1]
+            tree[i] = right
+            tree[i+1] = left
+    for j in range(0,len(tree)):
+        if len(tree[j])>1:
+            tree[j] = S_Transfer(tree[j],v1,v2)
         #recursive call
     return tree
 
-#test sentence
+"""
+#These were my mini tests!
 vp = Tree('VP', [Tree('V', ['saw']), Tree('NP', ['him'])])
 s = Tree('S', [Tree('NP', ['I']), vp])
+t = Tree.fromstring("(S (NP (D the) (NN dog)) (VP (V chased) (NP (D the) (JJ green) (NN cat))))")
+"""
 
-testSentence = S_Transfer(s,'NP','VP')
+#Implementing the ( Adjective Noun --> Noun Adjective ) rule
+s1=treebank.parsed_sents('wsj_0011.mrg')[3]
+first = ['JJ','JJR','JJS'] #all types of adjectives
+second = ['NN','NNS','NNP','NNPS'] #all types of nouns
+testSentence = S_Transfer(s1,first,second)
 print testSentence
+
+
+#//////////////////////////////////////////////////////////////////////////////////////////////////////
+#                                               EXTRA CREDIT
+#//////////////////////////////////////////////////////////////////////////////////////////////////////
+
+languageDict = {'*T*-12':'*T*-12','South':'Sur','Korea':'Corea',"'s":"'s",'boom':'auge','economic':'económico','which':'cual','began':'empezó','in':'en','1986':'1986','stopped':'detenido','this':'esta','year':'año','because':'porque','of':'de','prolonged':'prolongado','labor':'trabajo','disputes':'disputas',',':',','trade':'comercio','conflicts':'conflictos','and':'y','exports':'exportaciones','sluggish':'lento','.':'.'}
+
+def L_Transfer(d,tree):
+    print tree,tree[0],type(tree[0])
+    if str(tree[0]) in d.keys():
+        u = unicode(d[tree[0]], "utf-8")
+        tree[0] = u
+    else:
+        for i in range(0,len(tree)):
+            tree[i]=L_Transfer(d,tree[i]) #recursive call
+    return tree
+
+newSentence = L_Transfer(languageDict,testSentence)
+print newSentence
